139d0856e3c187c61e319d3b47443f9b7986ee81
zyu22 <zyu22@naver.com>
2024-11-22 10:53:15 +0900

    [markup] 전체적인 기본 구조 변경, tailwind css 변경

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import ChallengeView from '../views/ChallengeView.vue';
 import UserView from '../views/UserView.vue';
+import AdminView from '../views/AdminView.vue';
+import { ContainerWithChildren } from 'postcss/lib/container';
+import ChallengeBoardView from '@/components/admin/ChallengeBoard.vue';
 
 const routes = [
   {
@@ -19,10 +22,25 @@ const routes = [
     component : ChallengeView
   },
   {
-      path: '/user',
-      name: 'user',
-      component: UserView,
-    },
+    path: '/user',
+    name: 'user',
+    component: UserView,
+    children: [
+
+    ]
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    component: AdminView,
+    children: [
+      {
+        path: '/admin/challenge',
+        component: ChallengeBoardView
+      }
+      
+    ]
+  }
 ];
 
 const router = createRouter({
